5aa23e23bf3a8521a6b9e7f1a24e344904cf57d6
Isaac-Laguna <isaac_laguna4830@americana.edu.co>
2026-09-12 16:57:43 -0500

    Show inventory
    The function shows the products on the inventory, if there is no products it will simply tell to the user and do nothing else, also the new function "AskContinue" just do as it name says.
    Finally, I decided to initializate the list with some objects on it, I was frustated that every time I wanted to debbug I had to make some products.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿// https://epimient.github.io/Profundizaci-n_en_NET/#/clase-05/doc
 /*
  * To do: 1. Clear screen 2. fix ShowMenu function, it should not do validations.
+ * Could be useful: Console.WriteLine(price.ToString("C2"));
 */
-//products.Add(new object[] { "Test", 3m, 5 });
 
-List<object[]> products = new(5);
+List<object[]> products = new(5)
+{
+    new object[] { "Arroz", 2500m, 20 },
+    new object[] { "Leche", 4500m, 15 },
+    new object[] { "Pan", 3000m, 5 }
+};
 
 do
 {
@@ -14,6 +19,11 @@ do
     {
         case 1:
             RegisterProduct(products);
+            Console.Clear();
+
+            break;
+        case 2:
+            ShowInventory(products);
             Console.Clear();
 
             break;
@@ -186,4 +196,45 @@ static void RegisterProduct(List<object[]> list)
     quantity = ReadIntegrer("Ingrese la cantidad de productos (cantidad disponible).", 1, 1000);
 
     list.Add([ name, price, quantity ]);
+}
+static void ShowInventory(List<object[]> list)
+{
+    if (list.Count == 0)
+    {
+        ShowError("No hay productos registrados en el inventario para mostrar.");
+        AskContinue();
+        return;
+    }
+
+    int i;
+    string info;
+
+    Console.WriteLine("=======================================================");
+    Console.WriteLine(" INVENTARIO DE PRODUCTOS                               ");
+    Console.WriteLine("=======================================================");
+    Console.WriteLine("ID | Nombre del producto | Precio | Cantidad disponible");
+    Console.WriteLine("-------------------------------------------------------");
+
+    for (i = 0; i < list.Count; i++)
+    {
+        info = $"{i + 1} | {list[i][0]} | {list[i][1]:C2} | {list[i][2]}";
+        
+        if (Convert.ToInt32(list[i][2]) <= 5)
+        {
+            info += " | ¡Poco stock!";
+        }
+
+        Console.WriteLine(info);
+    }
+
+    Console.WriteLine("=======================================================");
+
+    AskContinue();
+}
+static void AskContinue()
+{
+    Console.WriteLine("Presione cualquier tecla para continuar.");
+    ShowInput();
+
+    Console.ReadKey();
 }
